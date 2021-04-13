aa02fa9fe331008309d31a47a301d7456222707d
Terry <terry86062@gmail.com>
2021-04-13 12:23:38 +0800

    UI + Presentation Module：MVVM: Reducing Boilerplate, Shifting Reusable Presentation Logic from Controllers into Cross-Platform (Stateful & Stateless) ViewModels, and Decoupling Presentation from UI Frameworks with Swift Generics

---
+++ EssentialFeed/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by 黃偉勛 Terry on 2021/4/12.
+//
+
+import Foundation
+
+final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+    
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
+    
+    func loadFeed() {
+        onLoadingStateChange?(true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onFeedLoad?(feed)
+            }
+            self?.onLoadingStateChange?(false)
+        }
+    }
+}
