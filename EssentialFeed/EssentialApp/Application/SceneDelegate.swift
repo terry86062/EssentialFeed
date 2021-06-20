8d484914423c18e66f0c7d60c468f64ba7ee8648
Terry <terry86062@gmail.com>
2021-06-20 10:56:18 +0800

    Main Module：Continuous Delivery and Deployment: Automating the App Deploy to App Store Connect

--- EssentialFeed/EssentialApp/Application/SceneDelegate.swift
+++ EssentialFeed/EssentialApp/Application/SceneDelegate.swift
@@ -33,11 +33,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
         
@@ -60,6 +58,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localImageLoader))))
+        
         window?.makeKeyAndVisible()
     }
     
