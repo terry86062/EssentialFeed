fbcd7294eaec417d87dccbd9afed0b69faa7f48f
Terry <terry86062@gmail.com>
2021-06-26 10:16:57 +0800

    Main Module：From Design Patterns to Universal Abstractions Using the Combine Framework

--- EssentialFeed/EssentialApp/Application/FeedViewAdapter.swift
+++ EssentialFeed/EssentialApp/Application/FeedViewAdapter.swift
@@ -9,9 +9,9 @@ import UIKit
 
 final class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
-    private let imageLoader: FeedImageDataLoader
-    
-    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+
+    init(controller: FeedViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.controller = controller
         self.imageLoader = imageLoader
     }
