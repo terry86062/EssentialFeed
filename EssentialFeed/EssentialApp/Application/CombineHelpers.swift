c02d28b0a3ee0c6096e81865f4ef40f76345139c
Terry <terry86062@gmail.com>
2021-06-26 17:20:09 +0800

    Bonus：#001 - [Image Comments API] From Dependency Injection to Dependency Rejection

--- EssentialFeed/EssentialApp/Application/CombineHelpers.swift
+++ EssentialFeed/EssentialApp/Application/CombineHelpers.swift
@@ -8,6 +8,22 @@
 import Foundation
 import Combine
 
+public extension HTTPClient {
+    typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
+    
+    func getPublisher(url: URL) -> Publisher {
+        var task: HTTPClientTask?
+        
+        return Deferred {
+            Future { completion in
+                task = self.get(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
 public extension FeedImageDataLoader {
     typealias Publisher = AnyPublisher<Data, Error>
     
@@ -38,7 +54,7 @@ private extension FeedImageDataCache {
     }
 }
 
-public extension FeedLoader {
+public extension LocalFeedLoader {
     typealias Publisher = AnyPublisher<[FeedImage], Error>
     
     func loadPublisher() -> Publisher {
--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -23,3 +23,14 @@ func anyData() -> Data {
 func uniqueFeed() -> [FeedImage] {
     return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
 }
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
